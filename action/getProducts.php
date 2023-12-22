9d44b4a5378e5962e4b6f1ad8004cdd0e049858a
MIKURI <and.iks9034@gmail.com>
2023-12-22 08:28:54 +0400

    (o ~ o)

--- action/getProducts.php
+++ action/getProducts.php
@@ -7,15 +7,6 @@ $data = Data::getProducts();
 $length = count($data);
 
 foreach($data as $index => $value){
-   if($index < $length - 1) { 
-        $response .= '{ 
-            "id": '.$index.',
-            "company": "'.$value[0].'",
-            "product": "'.$value[1].'",
-            "price": '.$value[2].',
-            "quant": '.$value[3].'
-        },';
-    } else {
         $response .= '{ 
             "id": '.$index.',
             "company": "'.$value[0].'",
@@ -23,7 +14,7 @@ foreach($data as $index => $value){
             "price": '.$value[2].',
             "quant": '.$value[3].'
         }';
-    }
+        $index < $length - 1 ? $response .= ",": null;
 };
 
 print_r("[".$response."]");
